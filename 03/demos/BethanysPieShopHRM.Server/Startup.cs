aedef6f3a3c97e4fa738361f1737bfd042de7835
Shenyi <baoshenyi@gmail.com>
2021-08-22 16:12:17 -0600

    change ports

--- 03/demos/BethanysPieShopHRM.Server/Startup.cs
+++ 03/demos/BethanysPieShopHRM.Server/Startup.cs
@@ -26,7 +26,7 @@ namespace BethanysPieShopHRM.Server
             
             services.AddScoped<HttpClient>(s =>
             {
-                var client = new HttpClient { BaseAddress = new System.Uri("https://localhost:44340/") }; 
+                var client = new HttpClient { BaseAddress = new System.Uri("https://localhost:5001/") }; 
                 return client;
             });
 
